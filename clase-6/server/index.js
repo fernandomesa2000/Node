9c5c49bbd89467dc86d41ee9fa76ff8cb2bddde7
Fernando Bedoya <juanfer30082000@gmail.com>
2025-09-21 18:22:10 -0500

    Clase 6 Chatt

---
+++ clase-6/server/index.js
@@ -0,0 +1,84 @@
+import express from 'express';
+import logger from 'morgan';
+import dotenv from 'dotenv';
+import { createClient } from '@libsql/client';
+
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+
+dotenv.config();
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
+
+const db = createClient({
+    url: "libsql://eternal-beak-fernandomesa2000.aws-us-east-1.turso.io",
+    authToken: process.env.DB_TOKEN,
+});
+
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        user TEXT
+    )
+`);
+
+io.on('connection', async (socket) => {
+    console.log('A user has connected!');
+
+    socket.on('disconnect', () => {
+        console.log('A user has disconnected!');
+    });
+
+    socket.on('chat message', async (msg) => {
+        let result
+        const username = socket.handshake.auth.username ?? 'anonymous';
+        try {
+            result = await db.execute({
+                sql: "INSERT INTO messages (content, user) VALUES (:msg, :username)",
+                args: { msg, username },
+            });
+        } catch (error) {
+            console.error(error);
+            return;
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username);
+    });
+
+    console.log('Auth 🔽');
+    console.log(socket.handshake.auth);
+
+    if (!socket.recovered) { //Recover messages withouth conection
+        try {
+            const results = await db.execute({
+                sql: "SELECT id, content, user FROM messages WHERE id > ?",
+                args: [socket.handshake.auth.serverOffset ?? 0],
+            })
+
+            results.rows.forEach((row) => {
+                socket.emit('chat message', row.content, row.id.toString(), username);
+            })
+        } catch (error) {
+            console.error(error);
+            return;
+        }
+    }
+})
+
+app.use(logger('dev'));
+// 🔹 Servir archivos estáticos (CSS, JS, imágenes)
+app.use(express.static(process.cwd() + '/client'));
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html');
+});
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
