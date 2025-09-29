6f676c52c51670b58938d4bfd3d28e6f089803a7
Fernando Bedoya <juanfer30082000@gmail.com>
2025-09-29 18:28:31 -0500

    cookies and tokens

---
+++ user-auth/user-repository.js
@@ -0,0 +1,61 @@
+import crypto from "node:crypto";
+
+import DBLocal from "db-local";
+import bcrypt from "bcrypt";
+
+import { SALT_ROUNDS } from "./config.js";
+
+const { Schema } = new DBLocal({ path: './db' });
+
+const User = Schema('User', {
+    _id: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+})
+
+export class UserRepository {
+    static async create({ username, password }) {
+        //1. Username validations (ZOD optional)
+        Validation.username(username);
+        Validation.password(password);
+        //2. Create User
+        const user = User.findOne({ username });
+        if (user) throw new Error('User already exists');
+
+        const id = crypto.randomUUID();
+        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS); // hashSync blocks principal thread
+
+        User.create({
+            _id: id,
+            username,
+            password: hashedPassword
+        }).save();
+
+        return id;
+    }
+    static async login({ username, password }) {
+        Validation.username(username);
+        Validation.password(password);
+
+        const user = User.findOne({ username });
+        if (!user) throw new Error(`User doesn't exist`);
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) throw new Error('Invalid password');
+
+        const { password: _, ...publicUser } = user;
+
+        return publicUser
+    }
+}
+
+class Validation {
+    static username(username) {
+        if (typeof username !== 'string') throw new Error('Username is not a string');
+        if (username.length < 3) throw new Error('Username is too short');
+    }
+    static password(password) {
+        if (typeof password !== 'string') throw new Error('Password is not a string');
+        if (password.length < 6) throw new Error('Password is too short');
+    }
+}
